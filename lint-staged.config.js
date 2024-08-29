a68fe4febaa49f33feaacfc88a2749ff50bcada8
bar haim <barha@il.ibm.com>
2024-08-29 11:34:07 +0300

    nextjs 12 + cc-components

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-    '**/*.ts?(x)': () => 'yarn check',
     '*.+(js|jsx|ts|tsx|json|css|md|mdx)': ['prettier --write'],
   };
